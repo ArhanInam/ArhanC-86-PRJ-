0459d79ebe193bde2c77458c9f10d7dfcff08d5a
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2023-04-01 15:17:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
 
+ 
+
 function new_image()
 {
-	fabric.Image.fromURL('BirthdayImage.jpg', finction(Img){
-        block_image_object=Img;
+	fabric.Image.fromURL("img4.jpg", function(Img) {
+        block_image_object = Img;
+
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
-        top:0,
-        left: 0
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        canvas.add(block_image_object);
     });
-    canvas.add(block_image_object);
 	
-};
+}
 
 function playSound(){
-	x.play();
-};
+    document.getElementById("myAudio").play();
+}
+
